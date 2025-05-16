4206368975330ef25e6f29a56cce692a4017fcb5
Rafael Sulimann <sulimannrafael@gmail.com>
2025-05-16 14:33:04 -0300

    Removing Setters of Usuario

--- architecture/clean-arch/src/main/java/br/com/sulimann/clean_arch/domain/usuario/Usuario.java
+++ architecture/clean-arch/src/main/java/br/com/sulimann/clean_arch/domain/usuario/Usuario.java
@@ -24,41 +24,20 @@ public class Usuario {
     return id;
   }
 
-  public void setId(String id) {
-    this.id = id;
-  }
-
   public String getNome() {
     return nome;
-  }
-
-  public void setNome(String nome) {
-    this.nome = nome;
   }
 
   public String getEmail() {
     return email;
   }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
-
   public Telefone getTelefone() {
     return telefone;
-  }
-
-  public void setTelefone(Telefone telefone) {
-    this.telefone = telefone;
   }
 
   public Endereco getEndereco() {
     return endereco;
   }
-
-  public void setEndereco(Endereco endereco) {
-    this.endereco = endereco;
-  }
-
 
 }
